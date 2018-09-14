dcf5c4f6a88ea7aba8171810b62a2d3e8e6e475d
evgenii <kotlyarov.ev@gmail.com>
2018-09-15 01:56:37 +0600

    added functions for mobile menu

--- landing_example/js/main.js
+++ landing_example/js/main.js
@@ -1,9 +1,48 @@
 $(document).ready(function(){
+  // Main screen slider
   $('.presentation').slick({
     autoplay: true,
     arrows: false,
     dots: true,
     dotsClass: 'dots',
     mobileFirst: true
+  });
+
+  // mobile menu
+  // make sure mobile menu is hidden on first appearance on mobile screens
+  var myLanding = {
+    mobileMenu: $('.menu'),
+    toggleButton: $('.menu-toggle'),
+    mobileMenuBreakpoint: 900
+  };
+  myLanding.hideMenu = function (e) {
+    myLanding.mobileMenu.hide();
+  };
+  myLanding.toggleMenu = function (e) {
+    myLanding.mobileMenu.slideToggle(300)
+  };
+  myLanding.showMenu = function (e) {
+    myLanding.mobileMenu.show();
+  };
+  myLanding.toggleMenuOnWindowResize = function (e) {
+    if(window.innerWidth > myLanding.mobileMenuBreakpoint) {
+      myLanding.showMenu();
+      myLanding.toggleButton.addClass('open');
+    } else {
+      myLanding.hideMenu();
+      myLanding.toggleButton.removeClass('open')
+    }
+  };
+
+  myLanding.toggleMenuOnWindowResize();
+
+  myLanding.toggleButton.click(function (e) {
+    e.preventDefault();
+    myLanding.toggleButton.toggleClass('open');
+    myLanding.toggleMenu();
+  });
+
+  $(window).resize(function (e) {
+    myLanding.toggleMenuOnWindowResize();
   });
 });
