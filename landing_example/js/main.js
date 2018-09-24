39223e5581b13087a8950dda60fd47dcd4a7d5c7
evgenii <kotlyarov.ev@gmail.com>
2018-09-24 23:36:34 +0600

    fixed slick slider

--- landing_example/js/main.js
+++ landing_example/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function(){
   // Main screen slider
   $('.presentation').slick({
     autoplay: true,
@@ -20,7 +20,7 @@ $(document).ready(function () {
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 320,
+        breakpoint: 400,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1
@@ -35,7 +35,7 @@ $(document).ready(function () {
       },
       {
         breakpoint: 900,
-        settings: {
+        settings:  {
           slidesToShow: 5,
           slidesToScroll: 1,
           dots: false
@@ -49,7 +49,6 @@ $(document).ready(function () {
     autoplay: false,
     arrows: false,
     dots: true,
-    variableWidth: true,
     slidesToShow: 4,
     slidesToScroll: 2,
     dotsClass: 'dots-dark',
@@ -57,27 +56,29 @@ $(document).ready(function () {
       {
         breakpoint: 900,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2
+          slidesToShow: 3
         }
       },
       {
         breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
+        settings:  {
+          slidesToShow: 2
         }
       },
 
       {
         breakpoint: 450,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+        settings:  {
+          slidesToShow: 1
         }
       }
     ]
   });
+
+// $(window).on('orientationchange', function() {
+//   $('.js-slider').slick('resize');
+// });
+
   // mobile menu
   // make sure mobile menu is hidden on first appearance on mobile screens
   var myLanding = {
@@ -85,80 +86,93 @@ $(document).ready(function () {
     toggleButton: $('.menu-toggle'),
     mobileMenuBreakpoint: 1000
   };
-  myLanding.hideMenu = function (e) {
+  myLanding.hideMenu = function(e){
     myLanding.mobileMenu.hide();
   };
-  myLanding.toggleMenu = function (e) {
-    myLanding.mobileMenu.slideToggle(300)
+  myLanding.toggleMenu = function(e){
+    myLanding.mobileMenu.slideToggle(300);
   };
-  myLanding.showMenu = function (e) {
+  myLanding.showMenu = function(e) {
     myLanding.mobileMenu.show();
   };
-  myLanding.toggleMenuOnWindowResize = function (e) {
-    if (window.innerWidth > myLanding.mobileMenuBreakpoint) {
+  myLanding.toggleMenuOnWindowResize = function(e){
+    if(window.innerWidth > myLanding.mobileMenuBreakpoint) {
       myLanding.showMenu();
       myLanding.toggleButton.addClass('open');
     } else {
       myLanding.hideMenu();
-      myLanding.toggleButton.removeClass('open')
+      myLanding.toggleButton.removeClass('open');
     }
   };
 
   myLanding.toggleMenuOnWindowResize();
 
-  myLanding.toggleButton.click(function (e) {
+  myLanding.toggleButton.click(function(e) {
     e.preventDefault();
     myLanding.toggleButton.toggleClass('open');
     myLanding.toggleMenu();
   });
 
-  $(window).resize(function (e) {
+  $(window).resize(function(e){
     myLanding.toggleMenuOnWindowResize();
   });
-  $(document).click(function (e) {
+
+  $(document).click(function(e) {
     var targetIsMenuOrButton = $(e.target).closest('.menu').length || !$(e.target).closest('.menu-toggle').length;
-    if (targetIsMenuOrButton && window.outerWidth < myLanding.mobileMenuBreakpoint) {
+    if(targetIsMenuOrButton && window.outerWidth < myLanding.mobileMenuBreakpoint) {
       myLanding.hideMenu();
       myLanding.toggleButton.removeClass('open');
     }
   });
-
   myLanding.stickyHeader = $('.sticky-header');
-  myLanding.minHeightOnScroll = window.outerHeight - 10;
+  myLanding.minHeightForScroll = window.outerHeight-10;
 
-  myLanding.toggleStickyMenu = function () {
-    var isScrollEnough = window.pageYOffset > myLanding.minHeightOnScroll;
-    if (isScrollEnough) {
+  myLanding.toggleStickyMenu = function(e) {
+    var isScrollEnough = window.pageYOffset > myLanding.minHeightForScroll;
+    if(isScrollEnough) {
       myLanding.stickyHeader.hide().addClass('sticked').show();
     } else {
       myLanding.stickyHeader.removeClass('sticked');
     }
   };
-
   myLanding.didScroll = false;
-  myLanding.toggleStickyHeader = function () {
+  myLanding.toggleStickyHeader = function() {
     myLanding.didScroll = true;
     myLanding.toggleStickyMenu();
-  };
 
-  $(window).scroll(function () {
-    myLanding.toggleStickyMenu()
-  });
+  };
+  window.onscroll = myLanding.toggleStickyHeader;
 
   // debounce scroll event
-  setInterval(function () {
-    if (myLanding.didScroll) {
-      myLanding.didScroll = true;
+  setInterval(function() {
+    if(myLanding.didScroll) {
+      myLanding.didScroll = false;
     }
   }, 100);
 
+
   // Modal
   myLanding.orderModal = $('.modal');
-  myLanding.modalToggle = $('[data-toggle="modal"]');
-  myLanding.modalToggle.click(function (e) {
+  myLanding.modalToggler = $('[data-toggle="modal"]');
+  myLanding.modalToggler.click(function(e){
     e.preventDefault();
     myLanding.orderModal.fadeToggle();
     $('body').toggleClass('is-modal-open');
   })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
