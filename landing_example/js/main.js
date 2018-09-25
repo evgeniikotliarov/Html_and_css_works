9500a315d94967ff4965266bf55e632a62639ffa
evgenii <kotlyarov.ev@gmail.com>
2018-09-25 23:08:56 +0600

    experiment with form

--- landing_example/js/main.js
+++ landing_example/js/main.js
@@ -150,7 +150,6 @@ $(document).ready(function(){
     }
   }, 100);
 
-
   // Modal
   myLanding.orderModal = $('.modal');
   myLanding.modalToggler = $('[data-toggle="modal"]');
@@ -158,6 +157,37 @@ $(document).ready(function(){
     e.preventDefault();
     myLanding.orderModal.fadeToggle();
     $('body').toggleClass('is-modal-open');
+  });
+
+  // upload file
+  myLanding.showFormSuccess = function () {
+    $('.modal-title h2').html("Thank you!");
+    $('#request_project').hide();
+  };
+  myLanding.showFormError = function () {
+    $('.modal-title h2').html("Something went wrong!");
+  };
+  document.getElementById('request_project').addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    var name = document.getElementById('name').value;
+    var phone_or_email = document.getElementById('phone_or_email').value;
+    var about_project = document.getElementById('about_project').value;
+    var file = document.getElementById('file_input').files[0];
+
+    var formData = new FormData();
+    formData.append('name', name);
+    formData.append('phone_or_email', phone_or_email);
+    formData.append('about_project', about_project);
+    formData.append('file', file);
+
+    fetch('http://localhost:63342/send', {
+      method: 'POST',
+      body: formData
+    }).then(function (value) {
+      myLanding.showFormSuccess();
+    }).catch(function (reason) {
+      myLanding.showFormError();
+    })
   })
 });
 
