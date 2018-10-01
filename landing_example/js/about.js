3ed3c08415f40417052d88b9020922052fbd83ba
evgenii <kotlyarov.ev@gmail.com>
2018-10-01 23:08:41 +0600

    add navbar

--- landing_example/js/about.js
+++ landing_example/js/about.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
   var pathname = window.location.pathname;
   $('.menu > a[href="' + pathname + '"]').addClass('active');
+
+  var myLanding = {
+    mobileMenu: $('.menu'),
+    toggleButton: $('.menu-toggle'),
+    mobileMenuBreakpoint: 900
+  };
+
+  // Modal
+  myLanding.orderModal = $('.modal');
+  myLanding.modalToggler = $('[data-toggle="modal"]');
+  myLanding.modalToggler.click(function (e) {
+    e.preventDefault();
+    myLanding.orderModal.fadeToggle();
+    $('body').toggleClass('is-modal-open');
+  });
 });
 
