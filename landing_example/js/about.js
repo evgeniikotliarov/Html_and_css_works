0e3fe910f1fb9abbc8ff8335a747ff0f5c95bfe2
evgenii <kotlyarov.ev@gmail.com>
2018-10-17 23:48:26 +0600

    Added toggle menu for mobile

--- landing_example/js/about.js
+++ landing_example/js/about.js
@@ -111,6 +111,62 @@ $(document).ready(function () {
     toggleButton: $('.menu-toggle'),
     mobileMenuBreakpoint: 900
   };
+  myLanding.hideMenu = function (e) {
+    myLanding.mobileMenu.hide();
+  };
+  myLanding.toggleMenu = function (e) {
+    myLanding.mobileMenu.slideToggle(300);
+  };
+  myLanding.showMenu = function (e) {
+    myLanding.mobileMenu.show();
+  };
+  myLanding.toggleMenuOnWindowResize = function (e) {
+    if (window.innerWidth > myLanding.mobileMenuBreakpoint) {
+      myLanding.showMenu();
+      myLanding.toggleButton.addClass('open');
+    } else {
+      myLanding.hideMenu();
+      myLanding.toggleButton.removeClass('open');
+    }
+  };
+
+  myLanding.toggleMenuOnWindowResize();
+
+  myLanding.toggleButton.click(function (e) {
+    e.preventDefault();
+    myLanding.toggleButton.toggleClass('open');
+    myLanding.toggleMenu();
+  });
+
+  $(window).resize(function (e) {
+    myLanding.toggleMenuOnWindowResize();
+  });
+
+  $(document).click(function (e) {
+    var targetIsMenuOrButton = $(e.target).closest('.menu').length || !$(e.target).closest('.menu-toggle').length;
+    if (targetIsMenuOrButton && window.innerWidth < myLanding.mobileMenuBreakpoint) {
+      myLanding.hideMenu();
+      myLanding.toggleButton.removeClass('open');
+    }
+  });
+  myLanding.stickyHeader = $('.home_page');
+  myLanding.minHeightForScroll = 300;
+
+  myLanding.toggleStickyMenu = function (e) {
+    var isScrollEnough = window.pageYOffset > myLanding.minHeightForScroll;
+    if (isScrollEnough) {
+      myLanding.stickyHeader.hide().addClass('sticked').show();
+    } else {
+      myLanding.stickyHeader.removeClass('sticked');
+    }
+  };
+  myLanding.didScroll = false;
+  myLanding.toggleStickyHeader = function () {
+    myLanding.didScroll = true;
+    myLanding.toggleStickyMenu();
+
+  };
+  window.onscroll = myLanding.toggleStickyHeader;
 
   // Modal
   myLanding.orderModal = $('.modal');
